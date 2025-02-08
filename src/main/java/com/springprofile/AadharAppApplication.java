745bf58296e10a8bb5b4d6001c139408821646b9
Krushnak915 <krushnak915@gmail.com>
2025-02-08 14:52:15 +0530

    first commit

---
+++ src/main/java/com/springprofile/AadharAppApplication.java
@@ -0,0 +1,13 @@
+package com.springprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadharAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadharAppApplication.class, args);
+	}
+
+}
